c8aeb6c93a8c833324f9f0b44de16762f550f9ae
luimas834 <luimas834@gmail.com>
2025-06-07 15:45:47 +0600

    Hero class updated

--- src/Hero.java
+++ src/Hero.java
@@ -1,30 +1,67 @@
 public class Hero extends Character {
     int specialCost;
     int specialDmg;
-    public Hero(int health,int power,int standardAtkDmg,int standardAtkPow,int specialCost,int specialDmg){
+    String heroType;
+    int weakenedAtk=0;
+    public Hero(int health,int power,int standardAtkDmg,int standardAtkPow,int specialCost,int specialDmg,String heroType){
         super(health,power,standardAtkDmg,standardAtkPow);
         this.specialCost=specialCost;
         this.specialDmg=specialDmg;
+        this.heroType=heroType;
     }
     
     public void normalAtk(Character target){
-        this.power-=standardAtkPow;
-        target.takeDamage(standardAtkDmg);
+        if(this.power>=this.standardAtkPow){
+            this.power-=standardAtkPow;
+            int damage=this.standardAtkDmg;
+
+            if(weakenedAtk!=0){
+                damage/=2;
+                weakenedAtk--;
+            }
+            
+            target.takeDamage(damage);
+            System.out.println(this.name+" used Attack on "+target.getName());
+            System.out.println(this.name+" caused "+damage+" damage.");
+        }
     }
 
+    //Mages ability
     public void castSpell(Character target){
-        this.power-=specialCost;
-        target.takeSpecialDamage(specialDmg);
+        if(this.power>=this.specialCost){
+            this.power-=specialCost;
+            target.takeSpecialDamage(specialDmg);
+            System.out.println(this.name+" used speical on "+target.getName());
+            System.out.println(this.name+" caused "+specialDmg+"damage.");
+        }
     }
 
+    //Archer ability
     public void quickShot(Character target){
-        this.power-=specialCost;
-        target.takeSpecialDamage(specialDmg);
+        if(this.power>=this.specialCost){
+            this.power-=specialCost;
+            target.takeSpecialDamage(specialDmg);
+            System.out.println(this.name+" used speical on "+target.getName());
+            System.out.println(this.name+" caused "+specialDmg+"damage.");
+        }
     }
 
+    //Warrior ability
     public void mightyStrike(Character target){
-        this.power-=specialCost;
-        target.takeSpecialDamage(specialDmg);
+       if(this.power>=this.specialCost){
+            this.power-=specialCost;
+            target.takeSpecialDamage(specialDmg);
+            System.out.println(this.name+" used speical on "+target.getName());
+            System.out.println(this.name+" caused "+specialDmg+"damage.");
+        }
+    }
+    
+    public void setWeakenedAtk(int count){
+        this.weakenedAtk=count;
+    }
+
+    public String getHeroType(){
+        return heroType;
     }
     
 }
